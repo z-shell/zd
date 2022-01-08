71c9c40ecc3de36901424fd2a8a4d94f512f4e50
Salvydas Lukosius <sall@w-ss.io>
2022-01-08 15:15:54 +0000

    Update init.zsh

--- docker/init.zsh
+++ docker/init.zsh
@@ -1,18 +1,10 @@
-# Fix permissions on /data
-if [[ -z "$QUIET" ]]
-then
-  echo "Setting owner of /data to ${PUID}:${PGID}" >&2
-fi
-
+printf '%s\n' "Setting owner of /data to ${PUID}:${PGID}" >&2
 sudo chown "${PUID}:${PGID}" /data
 sudo chown -R "${PUID}:${PGID}" /data
 
 # sync files between /data-static and /data
 if [[ -z "$NOTHING_FANCY" ]]
 then
-  if [[ -z "$QUIET" ]]
-  then
-    echo "Copying files from /data-static to /data" >&2
-  fi
+  printf '%s\n' "Copying files from /data-static to /data" >&2
   rsync -raq /data-static/ /data
 fi
