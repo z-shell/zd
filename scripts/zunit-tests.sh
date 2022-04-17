691d53f509f439a553738cc4313b267d854f24a3
Salvydas Lukosius <sall@w-ss.io>
2022-04-17 09:41:24 +0100

    Update zunit-tests.sh

--- scripts/zunit-tests.sh
+++ scripts/zunit-tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
 
 run_tests() {
   cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)/.." || exit 9
