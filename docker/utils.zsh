9f28746b5c1ecdbf6d5bec2e719ec70c8550c303
Salvydas Lukosius <sall@w-ss.io>
2022-04-28 14:05:48 +0100

    Update utils.zsh

--- docker/utils.zsh
+++ docker/utils.zsh
@@ -18,7 +18,9 @@ zi::module() {
 }
 
 zi::init() {
-  source ~/.zi/bin/zi.zsh
+  if [[ -r "${XDG_CONFIG_HOME:-$HOME/.config}/zi/init.zsh" ]]; then
+    source "${XDG_CONFIG_HOME:-$HOME/.config}/zi/init.zsh" && zzinit
+  fi
 }
 
 zi::reload() {
