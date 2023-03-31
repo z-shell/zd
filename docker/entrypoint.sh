925632bdea478bf8cf063621f70eb620a906c874
Salvydas Lukosius <sall@w-ss.io>
2023-03-31 04:27:11 +0100

    ci: trunk config update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash -l
+#!/usr/bin/env sh
 
-export HOME=/home/${ZUSER}
+export HOME=/home/"${ZUSER}"
 
 command sed -ir 's#^(root:.+):/bin/ash#\1:/bin/zsh#' /etc/passwd
 command adduser -D -s /bin/zsh -u "${PUID}" -h "${HOME}" "${ZUSER}"
@@ -11,7 +11,7 @@ command mkdir -p /src /data && chown -R "${PUID}:${PGID}" /src /data
 command -sfv /src/zshenv "${HOME}"/.zshenv
 command -sfv /src/zshrc "${HOME}"/.zshrc
 
-if [[ -f ${HOME}/init.zsh ]]; then
+if [ -f "${HOME}"/init.zsh ]; then
   command chmod u+x "${HOME}"/init.zsh
   # shellcheck source=/dev/null
   . "${HOME}"/init.zsh
